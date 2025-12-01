d879ec0a893a64d6533a17041c29a1eb46f39659
김지환 <rlawl4240@gmail.com>
2025-12-01 09:13:22 +0900

    [FEAT] 특정 형질을 가지는 대장균 찾기 (LEVEL 1)

---
+++ 00_sql-analytics-practice/programmers/2025/11/Lv1_298516.sql
@@ -0,0 +1,8 @@
+-- Site   : Programmers
+-- Title  : 특정 형질을 가지는 대장균 찾기 (LEVEL 1)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/298516
+-- Date   : 2025-12-01
+
+SELECT COUNT(*) FISH_COUNT
+FROM FISH_INFO
+WHERE TIME BETWEEN '2021/01/01' AND '2021/12/31'
