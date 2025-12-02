3aab84203ed72977c37de731ae708f68a5646bab
김지환 <rlawl4240@gmail.com>
2025-12-02 10:19:25 +0900

    [FEAT] 상품 별 오프라인 매출 구하기 (Lv2)

---
+++ 00_sql-analytics-practice/programmers/2025/11/Lv1_298515.sql
@@ -0,0 +1,7 @@
+-- Site   : Programmers
+-- Title  : 잡은 물고기 중 가장 큰 물고기의 길이 구하기 (LEVEL 1)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/298515
+-- Date   : 2025-12-02
+
+SELECT concat(MAX(LENGTH), 'cm') AS MAX_LENGTH
+FROM FISH_INFO;
