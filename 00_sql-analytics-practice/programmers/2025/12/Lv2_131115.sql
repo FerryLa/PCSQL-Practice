c9fbab4915e40ba050667924cade629371546d76
김지환 <rlawl4240@gmail.com>
2025-12-07 16:22:59 +0900

    [FEAT] README 수정, 가격이 제일 비싼 식품의 정보 출력하기 (Lv_2)
    - README.md 수정
    - Weekly SQL solve Count 주차: 9
    - 가격이 제일 비싼 식품의 정보 출력하기 (Lv_2)

---
+++ 00_sql-analytics-practice/programmers/2025/12/Lv2_131115.sql
@@ -0,0 +1,13 @@
+-- Site   : Programmers
+-- Title  : 가격이 제일 비싼 식품의 정보 출력하기 (LEVEL 2)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/131115
+-- Date   : 2025-12-07
+
+
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+FROM FOOD_PRODUCT
+GROUP BY PRODUCT_ID
+ORDER BY PRICE DESC LIMIT 1;
+
+-- 서브쿼리와 MAX가 더 안정적
+-- LEVEL 3 문제 풀기
