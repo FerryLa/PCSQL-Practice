2933c31c1974e1bf715f6ecafb564a33c24285e4
김지환 <rlawl4240@gmail.com>
2025-12-07 21:21:21 +0900

    문제(title) 초기화

--- 00_sql-analytics-practice/programmers/2025/12/Lv1_298516.sql
+++ 00_sql-analytics-practice/programmers/2025/12/Lv1_298516.sql
@@ -1,7 +1,7 @@
 -- Site   : Programmers
 -- Title  : 특정 형질을 가지는 대장균 찾기 (LEVEL 1)
 -- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/298516
--- Date   : 2025-12-01
+-- Date   : 2025-12-01 
 
 SELECT COUNT(*) FISH_COUNT
 FROM FISH_INFO
