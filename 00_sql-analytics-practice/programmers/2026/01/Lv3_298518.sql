0e6c359739bb22f53f24f86c7caed27497aadf88
김지환 <rlawl4240@gmail.com>
2026-01-16 07:59:59 +0900

    [FIX] 특정 물고기를 잡은 총 수 구하기

--- 00_sql-analytics-practice/programmers/2026/01/Lv3_298518.sql
+++ 00_sql-analytics-practice/programmers/2026/01/Lv3_298518.sql
@@ -1,4 +1,3 @@
-
 -- Site   : Programmers
 -- Title  : 특정 물고기를 잡은 총 수 구하기 (LEVEL 2)
 -- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/298518
@@ -9,4 +8,5 @@ FROM FISH_INFO
 WHERE FISH_TYPE = '0' OR FISH_TYPE = '1'
 GROUP BY FISH_TYPE = '0' OR FISH_TYPE = '1'
 
--- 오답 복기 필요.
+-- 원인: 하드코딩, 단일집계 불가, JOIN문 생략으로 인한 이름 매핑 불가
+-- 해결: GROUP BY 삭제, 이름으로 필터링
