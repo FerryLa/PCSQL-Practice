0f9801b837e620dd51fde2cef5b9308dac0962b6
김지환 <rlawl4240@gmail.com>
2026-01-17 08:47:24 +0900

    {FIX] 특정 물고기를 잡은 총 수 구하기

--- 00_sql-analytics-practice/programmers/2026/01/Lv3_298518.sql
+++ 00_sql-analytics-practice/programmers/2026/01/Lv3_298518.sql
@@ -3,10 +3,15 @@
 -- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/298518
 -- Date   : 2026-01-16
 
-SELECT COUNT(*) AS FISH_COUNT
-FROM FISH_INFO
-WHERE FISH_TYPE = '0' OR FISH_TYPE = '1'
-GROUP BY FISH_TYPE = '0' OR FISH_TYPE = '1'
+-- SELECT COUNT(*) AS FISH_COUNT
+-- FROM FISH_INFO
+-- WHERE FISH_TYPE = '0' OR FISH_TYPE = '1'
+-- GROUP BY FISH_TYPE = '0' OR FISH_TYPE = '1'
 
 -- 원인: 하드코딩, 단일집계 불가, JOIN문 생략으로 인한 이름 매핑 불가
 -- 해결: GROUP BY 삭제, 이름으로 필터링
+
+SELECT COUNT(*) AS FISH_COUNT
+FROM FISH_INFO FI
+LEFT JOIN FISH_NAME_INFO FNI ON FI.FISH_TYPE = FNI.FISH_TYPE
+WHERE FNI.FISH_NAME IN ('BASS','SNAPPER')
