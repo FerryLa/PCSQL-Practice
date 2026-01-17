acd11ea170e63442b7cfddeb517595dfb5b0f352
김지환 <rlawl4240@gmail.com>
2026-01-17 10:11:33 +0900

    [FIX] 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기

--- 00_sql-analytics-practice/programmers/2026/01/Lv3_299305 copy.sql
+++ 00_sql-analytics-practice/programmers/2026/01/Lv3_299305 copy.sql
@@ -9,4 +9,17 @@ FROM FISH_INFO
 GROUP BY FISH_TYPE
 HAVING MAX_LENGH >= 33
 
--- 시간초과, 복기 필요: 
+-- 오답: 10cm이하(NULL) 물고기 조건 부합하지 않음, 33m이상인 최대길이
+-- 풀이: 평균 길이를 구하려면 10cm이하인 물고기가 NULL인 경우를 치환해서 풀어야함
+
+SELECT 
+    COUNT(*) AS FISH_COUNT,
+    MAX(IF(LENGTH IS NULL, 10, LENGTH)) AS MAX_LENGTH,
+    #IF구문 사용법 IF(조건, true인자, else인자)
+    FISH_TYPE
+FROM FISH_INFO
+GROUP BY FISH_TYPE
+HAVING AVG(LENGTH) >= 33
+ORDER BY FISH_TYPE;
+
+-- 결론: 시간에 쫓겨서 문제도 제대로 읽지 않고 풀다 말은 듯
