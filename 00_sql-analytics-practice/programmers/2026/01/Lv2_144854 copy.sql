5d84156427086092029b3da1abd4e802cc234bce
김지환 <rlawl4240@gmail.com>
2026-01-24 06:58:26 +0900

    [FEAT] 최솟값 구하기(

---
+++ 00_sql-analytics-practice/programmers/2026/01/Lv2_144854 copy.sql
@@ -0,0 +1,10 @@
+-- Site   : Programmers
+-- Title  : 최솟값 구하기(LEVEL 2)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/59038
+-- Date   : 2026-01-24
+
+
+SELECT MIN(DATETIME) '시간'
+FROM ANIMAL_INS;
+
+-- DATETIME의 최솟값은 가장 이른시간
