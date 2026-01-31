ebb4676cbee45790c31d849e88fe34ef04adc2ee
김지환 <rlawl4240@gmail.com>
2026-02-01 07:56:42 +0900

    [FIX] 오프라인/온라인 판매 데이터 통합하기
    - 복기

--- 00_sql-analytics-practice/programmers/2026/01/Lv4_131537.sql
+++ 00_sql-analytics-practice/programmers/2026/01/Lv4_131537.sql
@@ -2,13 +2,26 @@
 -- Title  : 오프라인/온라인 판매 데이터 통합하기 (LEVEL 4)
 -- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/131537
 -- Date   : 2026-01-25
+-- Modify : 2026-02-01
 
 
 SELECT 
-    DATE_FORMAT(NS.SALES_DATE, 
-    NS.PRODUCT_ID,
-    IFNULL(NS.USER_ID),
-FROM ONLINE_SALE NS
-JOIN OFFLINE_SALE FS ON NS.PRODUCT_ID = FS.PRODUCT_ID;
+    DATE_FORMAT(SALES_DATE, '%Y-%m-%d') SALES_DATE,
+    PRODUCT_ID,
+    USER_ID,
+    SALES_AMOUNT
+FROM ONLINE_SALE
+WHERE DATE_FORMAT(SALES_DATE, '%Y-%m') = '2022-03'
 
--- 열은 어떻게 합칠지 감이 안잡힘
+UNION ALL
+
+SELECT
+    DATE_FORMAT(SALES_DATE, '%Y-%m-%d') SALES_DATE,
+    PRODUCT_ID,
+    NULL AS USER_ID, # 그냥 만들어줄 수 있네
+    SALES_AMOUNT
+FROM OFFLINE_SALE
+WHERE DATE_FORMAT(SALES_DATE, '%Y-%m') = '2022-03'
+ORDER BY SALES_DATE ASC, PRODUCT_ID ASC, USER_ID ASC;
+
+-- UNION은 중복 제거, UNION ALL은 중복 포함(성능 좋음)
