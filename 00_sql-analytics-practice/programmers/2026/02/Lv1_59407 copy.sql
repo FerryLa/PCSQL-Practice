34d9f65dd2f85ea67d3f3b8e79637105561b97af
김지환 <rlawl4240@gmail.com>
2026-02-09 06:53:53 +0900

    [FIX] 동물의 아이디와 이름

--- 00_sql-analytics-practice/programmers/2026/02/Lv1_59407 copy.sql
+++
@@ -1,8 +0,0 @@
--- Site   : Programmers
--- Title  : 동물의 아이디와 이름 (LEVEL 1)
--- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/59403
--- Date   : 2026-02-06
-
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID;
