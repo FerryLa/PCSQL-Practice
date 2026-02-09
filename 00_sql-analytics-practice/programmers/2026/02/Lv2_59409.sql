f7f9eaa2c82f784a4959cc626da831d7a9c0e7c9
김지환 <rlawl4240@gmail.com>
2026-02-10 08:00:14 +0900

    [FIX] 중성화 여부 파악하기

--- 00_sql-analytics-practice/programmers/2026/02/Lv2_59409.sql
+++ 00_sql-analytics-practice/programmers/2026/02/Lv2_59409.sql
@@ -10,4 +10,13 @@ SELECT
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID;
 
--- 오답
+-- 오답: IF, LIKE 혼동
+
+SELECT
+    ANIMAL_ID,
+    NAME,
+    IF(SEX_UPON_INTAKE REGEXP 'Neutered|Spayed','O','X') '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMA_ID;
+
+-- CASE WHEN THEN보단 IF가 가독성이 좋음
