a5eb0a5f442499ad13f47e1c81f2a81e86404063
김지환 <rlawl4240@gmail.com>
2026-02-13 06:02:08 +0900

    [FEAT] 동물 수 구하기

---
+++ 00_sql-analytics-practice/programmers/2026/02/Lv2_59406.sql
@@ -0,0 +1,7 @@
+-- Site   : Programmers
+-- Title  : 동물 수 구하기 (LEVEL 2)
+-- Link   : https://school.programmers.co.kr/learn/courses/30/lessons/59406
+-- Date   : 2026-02-13
+
+SELECT COUNT(*) COUNT
+FROM ANIMAL_INS;
